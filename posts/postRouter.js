f81e39739e7276ac443fa10a702cfa5ade991c45
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-02-12 17:24:11 -0800

    working on finishing MVP

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,82 @@
 const express = require('express');
 
+const postDB = require('../posts/postDb');
+
 const router = express.Router();
 
+router.use(express.json());
+
+router.post('/:id', (req, res) => {
+  if(req.body){
+  postDB.insert(post)
+  .then(post => {
+    res.status(201).json(post)
+  })
+  .catch(error => {
+    res.status(500).json({errorMessage: "Something is wrong with the server"})
+  })
+  } else {
+    res.status(404).json({errorMessage: "Please enter all the data necesarry"})
+  }
+})
+
 router.get('/', (req, res) => {
-  // do your magic!
+  postDB.get()
+  .then(posts => {
+    res.status(200).json(posts)
+  })
+  .catch(error => {
+    res.status(500).json({errorMessage: "Something is wrong with the server"})
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  postDB.getById(req.params.id)
+  .then(post => {
+    console.log(post)
+    if(post){
+    res.status(200).json(post)
+    } else {
+      res.status(404).json({errorMessage: "Can't find user"})
+    }
+  })
+  .catch(error => {
+    res.status(500).json({errorMessage: "Something wrong with the server"})
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  postDB.getById(req.params.id)
+  .then(user => {
+    if(user){
+      postDB.remove(user.id)
+      .then(deleted => {
+        res.status(200).json({errorMessage: "User deleted"})
+      })
+    } else {
+      res.status(404).json({errorMessage: "Can't find that post"})
+    }
+  })
+  .catch(error => {
+    res.status(500).json({errorMessage: "Something wrong with the server"})
+})
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  postDB.getById(req.params.id)
+  .then(post => {
+    if(post){
+      postDB.update(req.params.id, req.body)
+      .then(deleted => {
+        res.status(200).json({errorMessage: "User Changed"})
+      })
+    } else {
+      res.status(404).json({errorMessage: "Can't find that post"})
+    }
+  })
+  .catch(error => {
+    res.status(500).json({errorMessage: "Something wrong with the server"})
+})
 });
 
 // custom middleware
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,96 @@
 const express = require('express');
 
+const userDB = require('./userDb')
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
-});
+router.use(express.json());
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/', (req, res) => {
+  userDB.insert(req.body).then(user => {
+  if(user){
+    res.status(200).json({successMessage: "User Added"})
+  } else {
+    res.status(404).json({errorMessage: "Please enter a name"})
+  }
+  })
+  .catch(error => {
+    res.status(500).json({errorMessage: "Something is wrong with the server"})
+  })
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  userDB.get()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(error => {
+    res.status(500).json({errorMessage: "Something is wrong with the server"})
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  userDB.getById(req.params.id)
+  .then(post => {
+    if(post){
+    res.status(200).json(post)
+    } else {
+      res.status(404).json({errorMessage: "Can't find user"})
+    }
+  })
+  .catch(error => {
+    res.status(500).json({errorMessage: "Something wrong with the server"})
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  userDB.getById(req.params.id)
+  .then(user => {
+    userDB.getUserPosts(user.id)
+    .then(posts => {
+      if(posts){
+        res.status(200).json(posts)
+      } else {
+        res.status(500).json({errorMessage: "Something wrong with the server"})
+      }
+    })
+  })
+  .catch(error => {
+    res.status(404).json({errorMessage: "Couldn't find that post"})
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  userDB.getById(req.params.id)
+  .then(user => {
+    if(user){
+      userDB.remove(user.id)
+      .then(res => {
+      res.status(200).json({errorMessage: "User deleted"})
+      })
+    } else {
+      res.status(404).json({errorMessage: "Can't find that user"})
+    }
+  })
+  .catch(error => {
+    res.status(500).json({errorMessage: "Something wrong with the server"})
+})
 });
 
+
 router.put('/:id', (req, res) => {
-  // do your magic!
+  userDB.getById(req.params.id)
+  .then(user => {
+    if(user){
+       userDB.update(req.params.id, req.body)
+       .then(res.status(200).json({successMessage: "User Changed"}))
+    } else {
+      res.status(404).json({errorMessage: "Can't find that user"})
+    }
+  })
+  .catch(error => {
+    res.status(500).json({errorMessage: "Something wrong with the server"})
+  })
 });
 
 //custom middleware
